56bd4c5fdae6afd57a026213e0e4dce1408cef28
alesandramempis <alesandramempis@gmail.com>
2021-09-01 13:20:41 -0300

    Soma e filtragem de valores impares usando filter e reduce

--- exercicios.js
+++ exercicios.js
@@ -11,8 +11,17 @@ console.log('O somatório dos valores', valores, 'é:', somaDosvalores);
 //2) Dado um vetor de números, como poderia ser realizada a soma de todos os valores pares utilizando reduce e filter
 
 const calculoDeValoresPares = item => item % 2 === 0;
-const valoresPares = valores.filter(somenteValoresPares);
+const valoresPares = valores.filter(calculoDeValoresPares);
 
 const filtrarValoresPares = valores.filter(calculoDeValoresPares).reduce(operadorDeSoma);
 
-console.log('Os valores pares do array', valoresPares, 'são:', pares,'e a soma é:', filtrarValoresPares)
+console.log('Os valores pares do array', valores, 'são:', valoresPares,'e a soma é:', filtrarValoresPares);
+
+// 3) Dado um vetor de números, como poderia ser realizada a soma de todos os valores ímpares utilizando reduce e filter.
+
+const calculoDeValoresImpares = item => item % 2 !== 0;
+const valoresImpares = valores.filter(calculoDeValoresImpares)
+
+const filtrarValoresImpares = valores.filter(calculoDeValoresImpares).reduce(operadorDeSoma);
+
+console.log('Os valores impares do array', valores, 'são:', valoresImpares, 'e a soma é:', filtrarValoresImpares);
